75a93b8af7cb297caeea4e6e93315e8ac629674a
ekater1na <ekaterina_l@tut.by>
2020-02-19 18:49:44 +0300

    feat: add toggling filtering-criteria

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public showFilter: boolean = false;
 
   constructor() { }
 
   public ngOnInit(): void {
   }
 
+  public onShowFilter(): void {
+    this.showFilter = !this.showFilter;
+  }
 }

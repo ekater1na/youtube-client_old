3ec159035d19154e7df027973613972a286cf1e8
ekater1na <ekaterina_l@tut.by>
2020-02-28 15:54:52 +0300

    feat: add router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { DetailedInfoComponent } from './detailed-info/detailed-info.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: SearchResultsComponent},
+  {path: 'login', component: LoginComponent },
+  {path: 'details', component: DetailedInfoComponent },
+  {path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

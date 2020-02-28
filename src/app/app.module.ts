79ca73fe9a7a414500197a876cf76e8ca6a3f299
ekater1na <ekaterina_l@tut.by>
2020-02-28 13:38:46 +0300

    build: add  modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatFormFieldModule} from '@angular/material';
+import { MatInputModule} from '@angular/material';
 import { SetBorderDirective } from './directives/set-border.directive';
 import { FilterPipe } from './pipes/filter.pipe';
 import { SortPipe } from './pipes/sort.pipe';
@@ -40,6 +41,7 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatIconModule,
     MatGridListModule,
+    MatInputModule,
     FormsModule
   ],
   providers: [],
